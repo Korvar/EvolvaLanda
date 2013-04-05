42dc07c686cd186d0781bddf07c694a51aac0f23
Korvar <korvarthefox@gmail.com>
2013-04-05 13:26:35 +0100

    Simple Box2d demo created

---
+++ source/B2FlxSprite.hx
@@ -0,0 +1,74 @@
+package ;
+
+import box2D.collision.shapes.B2PolygonShape;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2BodyDef;
+import box2D.dynamics.B2FixtureDef;
+import box2D.dynamics.B2World;
+import org.flixel.FlxSprite;
+
+/**
+ * ...
+ * @author Mike Cugley
+ */
+class B2FlxSprite extends FlxSprite
+{
+	private var ratio:Float = 30.0;
+	
+	private var _fixDef:B2FixtureDef;
+	private var _bodyDef:B2BodyDef;
+	private var _obj:B2Body;
+	
+	private var _world:B2World;
+	
+	public var _friction:Float = 0.8;
+	public var _restitution:Float = 0.3;
+	public var _density:Float = 0.7;
+	
+	public var _angle:Float = 0;
+	
+	public function new(X:Float=0, Y:Float=0, Width:Float, Height:Float, w:B2World, SimpleGraphic:Dynamic=null) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+		width = Width;
+		height = Height;
+		_world = w;
+		
+	}
+	
+	override public function update():Void
+	{
+		x = (_obj.getPosition().x * ratio) -width / 2;
+		y = (_obj.getPosition().y * ratio) -height / 2;
+		angle = _obj.getAngle() * (180 / Math.PI);
+		
+		super.update();
+	}
+	
+	public function createBody():Void
+	{
+		var boxShape:B2PolygonShape = new B2PolygonShape();
+		boxShape.setAsBox((width / 2) / ratio, (height / 2) / ratio);
+		
+		_fixDef = new B2FixtureDef();
+		_fixDef.density = _density;
+		_fixDef.restitution = _restitution;
+		_fixDef.friction = _friction;
+		_fixDef.shape = boxShape;
+		
+		_bodyDef = new B2BodyDef();
+		_bodyDef.position.set((x + (width / 2)) / ratio, (y + (height / 2)) / ratio);
+		_bodyDef.angle = angle * (Math.PI / 180);
+		_bodyDef.type = B2Body.b2_dynamicBody;
+		
+		_obj = _world.createBody(_bodyDef);
+		_obj.createFixture(_fixDef);
+	}
+	
+	override public function kill():Void
+	{
+		_world.destroyBody(_obj);
+		super.kill();
+	}
+}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,17 @@
 package ;
 
+import org.flixel.FlxG;
 import org.flixel.FlxState;
+
+
+import box2D.collision.shapes.B2CircleShape;
+import box2D.collision.shapes.B2PolygonShape;
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2BodyDef;
+import box2D.dynamics.B2DebugDraw;
+import box2D.dynamics.B2FixtureDef;
+import box2D.dynamics.B2World;
 
 /**
  * ...
@@ -8,6 +19,8 @@ import org.flixel.FlxState;
  */
 class PlayState extends FlxState
 {
+	
+	public var _world:B2World;
 
 	public function new() 
 	{
@@ -19,9 +32,23 @@ class PlayState extends FlxState
 	{
 		super.create();
 		
+		setupWorld();
+		
 		createLandscape();
 		
 		createLander();
+		
+		var floor:B2FlxTileblock = new B2FlxTileblock(0, 400, 640, 80, _world);
+		floor.createBody();
+		floor.makeGraphic(640, 80, 0xFFFF9900);
+		add(floor);
+		
+		var cube:B2FlxSprite = new B2FlxSprite(320, 240, 20, 20, _world);
+		cube.angle = 30;
+		cube.createBody();
+		cube.makeGraphic(20, 20, 0xff00cccc);
+		
+		add(cube);
 
 	}
 
@@ -36,4 +63,16 @@ class PlayState extends FlxState
 		
 	}
 	
+	private function setupWorld():Void
+	{
+	var gravity:B2Vec2 = new B2Vec2(0, 9.8);
+	_world = new B2World(gravity, true);
+		
+	}
+	
+	override public function update():Void
+	{
+		_world.step(FlxG.elapsed, 10, 10);
+		super.update();
+	}
 }
